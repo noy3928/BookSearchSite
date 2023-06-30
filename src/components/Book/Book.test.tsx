74ceccba9924c6ad8357164571cc36d734cd0db3
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-06-30 23:58:16 +0900

    feat : write Book component just for passing test

--- src/components/Book/Book.test.tsx
+++ src/components/Book/Book.test.tsx
@@ -7,20 +7,19 @@ describe("Book", () => {
   beforeEach(() => render(<Book info={book} />))
 
   it("책 정보들이 화면에 로드되어야한다.", () => {
+    const imgElement = screen.getByRole("img")
+
     expect(
       screen.getByText("MongoDB in Action, 2nd Edition")
     ).toBeInTheDocument()
     expect(screen.getByText("Covers MongoDB version 3.0")).toBeInTheDocument()
-    expect(
-      screen.getByText("https://itbook.store/img/books/9781617291609.png")
-    ).toBeInTheDocument()
-    expect(
-      screen.getByText("https://itbook.store/books/9781617291609")
-    ).toBeInTheDocument()
+    expect(imgElement.getAttribute("src")).toBe(
+      "https://itbook.store/img/books/9781617291609.png"
+    )
   })
 
   it("Book 컴포넌트를 클릭하면, detail 페이지로 이동한다", () => {
-    const linkElement = screen.getByRole("link", { name: book.title })
+    const linkElement = screen.getByRole("link")
     expect(linkElement).toBeInTheDocument()
     expect(linkElement.getAttribute("href")).toBe(`/detail/${book.id}`)
   })
