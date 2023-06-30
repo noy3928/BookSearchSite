6b017bbe8f9a12978812374e04887f0e0c420ff2
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-06-30 22:46:37 +0900

    test : write test code for resolveKeyword util

---
+++ src/services/utils/tests/resolveKeyword.test.ts
@@ -0,0 +1,23 @@
+import { resolveKeyword } from "./utils"
+
+describe("resolveKeyword", () => {
+  it('"|" 연산자가 포함되어있으면 "or" 타입을 반환해야한다.', () => {
+    const result = resolveKeyword("tdd|javascript")
+    expect(result).toEqual({ type: "or", keywords: ["tdd", "javascript"] })
+  })
+
+  it('"-" 연산자가 포함되어있으면 "not" 타입을 반환해야 한다.', () => {
+    const result = resolveKeyword("tdd-javascript")
+    expect(result).toEqual({ type: "not", keywords: ["tdd", "javascript"] })
+  })
+
+  it('어떤 연산자도 포함되어있지 않으면 "none" 타입을 반환해야한다.', () => {
+    const result = resolveKeyword("tdd")
+    expect(result).toEqual({ type: "none", keywords: ["tdd"] })
+  })
+
+  it("빈 값이면 null을 반환한다.", () => {
+    const result = resolveKeyword("")
+    expect(result).toEqual(null)
+  })
+})
