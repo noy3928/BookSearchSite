f8b9f7a9161b8ab78eccd37c9ef3a724d14cccc7
yechan <noy3928@naver.com>
2023-07-01 22:15:55 +0900

    fix : modify feature of filterKeyword

--- src/services/utils/filterKeyword.ts
+++ src/services/utils/filterKeyword.ts
@@ -2,7 +2,7 @@ import { Book } from "@/shared/types/book"
 
 export const filterKeyword = (keyword: string, books: Book[]) => {
   const filteredBooks = books.filter(book => {
-    return book.title.includes(keyword)
+    return !book.title.includes(keyword)
   })
 
   return filteredBooks
--- src/services/utils/tests/filterKeyword.test.ts
+++ src/services/utils/tests/filterKeyword.test.ts
@@ -2,13 +2,8 @@ import { filterKeyword } from "../"
 import { books } from "@/shared/fixtures/book"
 
 describe("filterKeyword", () => {
-  it("매칭되는 키워드가 없으면 빈 배열을 반환한다.", () => {
-    const filteredBooks = filterKeyword("foo", books)
-    expect(filteredBooks).toEqual([])
-  })
-
-  it("매칭되는 키워드가 있는 값만 반환한다.", () => {
+  it("매칭되는 키워드를 제외한 값만 반환한다.", () => {
     const filteredBooks = filterKeyword("Mongo", books)
-    expect(filteredBooks).toEqual([books[0]])
+    expect(filteredBooks).toEqual([books[1], books[2], books[3]])
   })
 })
