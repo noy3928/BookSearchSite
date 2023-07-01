89fda8244e8f357a60e3aff4719946a8d4264248
yechan <noy3928@naver.com>
2023-07-01 23:08:14 +0900

    test : write test code for searchAndSetBooks util

---
+++ src/services/utils/tests/searchAndSetBooks.test.ts
@@ -0,0 +1,32 @@
+import { searchAndSetBooks } from "../"
+import { resolveKeyword, fetchBooksByType } from "@/services/utils"
+import { Book } from "@/shared/types/book"
+
+jest.mock("@/services/utils", () => ({
+  resolveKeyword: jest.fn(),
+  fetchBooksByType: jest.fn(),
+}))
+
+describe("searchAndSetBooks", () => {
+  const setState = jest.fn()
+  const keyword = "test"
+  const resolvedKeyword = "resolvedTest"
+  const books: Book[] = []
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it("주어진 함수들이 올바른 인자들로 호출된다. ", async () => {
+    ;(resolveKeyword as jest.Mock).mockResolvedValue(resolvedKeyword)
+    ;(fetchBooksByType as jest.Mock).mockResolvedValue(books)
+
+    const search = searchAndSetBooks(setState)
+
+    await search(keyword)
+
+    expect(resolveKeyword).toHaveBeenCalledWith(keyword)
+    expect(fetchBooksByType).toHaveBeenCalledWith(resolvedKeyword)
+    expect(setState).toHaveBeenCalledWith(books)
+  })
+})
