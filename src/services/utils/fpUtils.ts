187701aaffce1c296e8abffb4e4081529b7687cb
yechan <noy3928@naver.com>
2023-07-01 23:11:29 +0900

    feat : write pipe function

--- src/services/utils/fpUtils.ts
+++ src/services/utils/fpUtils.ts
@@ -2,3 +2,13 @@ export const filter =
   <T>(condition: (value: T) => boolean) =>
   (arr: T[]): T[] =>
     arr.filter(condition)
+
+type Function = (...args: any[]) => Promise<any> | any
+
+export const pipe =
+  (...fns: Function[]) =>
+  async (arg: any) =>
+    fns.reduce(async (prevPromise: Promise<any>, fn: Function) => {
+      const c = await prevPromise
+      return fn(c)
+    }, Promise.resolve(arg))
