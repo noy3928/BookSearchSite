4fed2978f35e86371e37f1d90d81400a2064cec6
yechan <noy3928@naver.com>
2023-07-03 20:02:44 +0900

    feat : implement searchAndSetBooks and remove test code temporary

--- src/services/utils/searchAndSetBooks.ts
+++ src/services/utils/searchAndSetBooks.ts
@@ -1,9 +1,16 @@
-import React from "react"
 import { pipe } from "./fpUtils"
 import { resolveKeyword, fetchBooksByType } from "@/services/utils"
 import { Book } from "@/shared/types/book"
-
 export const searchAndSetBooks =
-  (setState: React.Dispatch<React.SetStateAction<Book[]>>) =>
-  (keyword: string) =>
-    pipe(resolveKeyword, fetchBooksByType, setState)(keyword)
+  (setState: (books: Book[]) => void) =>
+  (loadingDecorator: (func: Function) => (args: any) => Promise<any>) =>
+  (setTotalBooks: (total: number) => void) =>
+  (pageNumber: number) =>
+  (keyword: string) => {
+    console.log(fetchBooksByType)
+    pipe(
+      resolveKeyword,
+      loadingDecorator(fetchBooksByType(pageNumber)(setTotalBooks)),
+      setState
+    )(keyword)
+  }
